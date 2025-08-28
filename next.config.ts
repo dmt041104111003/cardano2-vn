b219488bb3cd8c300eccd318da5829aefe1ec2c5
dmt041104111003 <daomanhtung4102003@gmail.com>
2025-08-29 02:09:06 +0700

    performance

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,32 @@ const config: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    outputFileTracingRoot: process.cwd(),
+    outputFileTracingExcludes: {
+      "*": [
+        "node_modules/@swc/core-linux-x64-gnu",
+        "node_modules/@swc/core-linux-x64-musl",
+        "node_modules/@esbuild/linux-x64",
+        "node_modules/@prisma/client/libquery_engine-*",
+        "node_modules/prisma/libquery_engine-*",
+        "node_modules/prisma/migration-engine-*",
+        "node_modules/prisma/introspection-engine-*",
+        "node_modules/prisma/prisma-fmt-*"
+      ]
+    }
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        'utf-8-validate': 'commonjs utf-8-validate',
+        'bufferutil': 'commonjs bufferutil',
+      });
+    }
+    return config;
+  },
+  output: 'standalone',
 };
 
 export default withMDX(config);
