e25ec85840f83904aaabf4d762256d6a00ecb606
dmt041104111003 <daomanhtung4102003@gmail.com>
2025-08-30 10:56:36 +0700

    reset

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import { createMDX } from "fumadocs-mdx/next";
-import CopyWebpackPlugin from "copy-webpack-plugin";
+
 import type { NextConfig } from "next";
 
 const withMDX = createMDX();
@@ -8,9 +8,18 @@ const config: NextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "res.cloudinary.com" },
-      { protocol: "https", hostname: "img.youtube.com" },
-      { protocol: "https", hostname: "i.ytimg.com" },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
     ],
     domains: ["res.cloudinary.com"],
   },
@@ -19,14 +28,6 @@ const config: NextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  webpack(config) {
-    config.plugins.push(
-      new CopyWebpackPlugin({
-        patterns: [{ from: "src/sw.js", to: "public" }],
-      })
-    );
-    return config;
   },
 };
 
